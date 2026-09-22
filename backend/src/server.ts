9bbcd831d2ba1880117edaf938f88bf599f01302
luisguassu-create <luis.guassu@aluno.senai.br>
2026-09-22 19:02:23 -0300

    configura backend para deploy na Vercel

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,10 @@ import app from "./app";
 
 const PORT = 3001;
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+  });
+}
+
+export default app;
